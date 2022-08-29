39db47991e3b7a2c3cb2037b38e2ac4d2a3f81eb
Rizano1 <ershanoano@gmail.com>
2022-08-29 14:33:32 +0700

    mentok membuat sistem super admin login lanjut setup orm

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const jsonParser = bodyParser.json()
 const app = express()
+
+app.use('/css', express.static(__dirname+'/css'))
+app.use('/assets', express.static(__dirname+'/assets'))
+app.use('/bootstrap-5.2.0-beta1-dist', express.static(__dirname + '/bootstrap-5.2.0-beta1-dist'))
+
+app.set('view engine', 'ejs')
 
 app.get('/',(req, res) => {
     res.send('hello world')
 })
 
 app.get('/login',(req, res) => {
-    
+    res.render("login", { css: "css/login.css" })
+})
+
+app.post('/login',(req, res) => {
+    const username = Ano
+    const password = Rizano789
+    if(password == req.body.password && username == req.body.username){
+        res.send("Authorized")
+      }else{
+        res.status(401).send("Unauthorized")
+      }
 })
 
 app.listen(3000, () => {
